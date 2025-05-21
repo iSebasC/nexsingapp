e30378cc5ae9b7031e1c3cc6ecc16f6ee62c5218
iSebasC <sebas.cabrera.456@gmail.com>
2025-05-20 22:17:09 -0500

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      hostname: "https://nexsigndev.com",
+    }),
+  ],
 });
